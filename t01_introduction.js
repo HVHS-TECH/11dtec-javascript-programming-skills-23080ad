4cc923fd8e41955a0e68c197f6879a42a84436fe
23080ad <23080ad@hvhs.school.nz>
2025-04-28 21:42:49 +0000

    Apr 28, 2025, 9:42 PM

--- t01_introduction.js
+++ t01_introduction.js
@@ -1,5 +1,8 @@
 /*****************************
- Name of task:
+ Name of task: Intro to JS and setting up your .js file.
+ Author: Angus. 
+ Date: Week 1 term 2 2025.   
+ purpose: Practice task.
  ****************************/
 console.log("Running t01_introduction.js");
 
