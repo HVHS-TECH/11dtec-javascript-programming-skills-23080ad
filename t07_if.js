eee20ab8e5d5408db5bbb65c718434c512064f36
23080ad <23080ad@hvhs.school.nz>
2025-05-01 01:27:14 +0000

    May 1, 2025, 1:27 AM

--- t07_if.js
+++ t07_if.js
@@ -11,7 +11,7 @@ let yourAge = prompt("how old are you");
 /*****************************
 Main code
 *****************************/
-if (yourAge > 70) {
+if (yourAge => 70) {
     alert = ("you are perfectly aged");
 } else {
     alert = ("you are too old!");
