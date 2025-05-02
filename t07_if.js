2f78f85cf811e51b1550f862ce962c33195e2a91
23080ad <23080ad@hvhs.school.nz>
2025-05-02 02:39:34 +0000

    May 2, 2025, 2:39 AM

--- t07_if.js
+++ t07_if.js
@@ -12,9 +12,11 @@ let yourAge = prompt("how old are you");
 Main code
 *****************************/
 if (yourAge >= 70) {
-    alert("you are perfectly aged")
+    alert("you are perfectly aged!")
+} else if (yourAge = 50){
+    alert("you are half a century old!")
 } else {
-    alert("you are too young!")
+    alert("you are worthless to me!")
 }
 /*****************************
 Functions 
