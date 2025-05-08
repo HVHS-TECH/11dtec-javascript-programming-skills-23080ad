3a250ace7316771f035b6513655b4404b32b038e
23080ad <23080ad@hvhs.school.nz>
2025-05-08 01:23:44 +0000

    May 8, 2025, 1:23 AM

--- t08_for_loop.js
+++ t08_for_loop.js
@@ -8,16 +8,18 @@
 
 
 //Variables
-var total;
 let numberAskA = 0;
 let numberAskB = 0;
+var total;
 /*****************************
 Main code
 *****************************/
 console.log("Running t08_for_loop.js");
-for (count = 0; count < 5; count++) {
+for (count = 0; count < 6; count++) {
     let numberAskA = prompt("give me a number!");
+    numberAskA = Number(numberAskA);
     let numberAskB = prompt("give me a number!");
+    numberAskB = Number(numberAskB);
     total = numberAskB + numberAskA;
     console.log(total);
 }
