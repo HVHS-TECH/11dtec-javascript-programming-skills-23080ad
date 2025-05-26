9a7e7fcf9607f0e68c00c9c2af7164d6fbaff972
23080ad <23080ad@hvhs.school.nz>
2025-05-26 21:20:49 +0000

    wloops

---
+++ t12_while_loop.js
@@ -0,0 +1,27 @@
+/*****************************
+ Name of task: Intro to JS and setting up your .js file.
+ Author: Angus. 
+ Date: Week 5 term 2 2025.   
+ purpose: Practice task.
+ ****************************/
+console.log("Running t12_whilw_loop.js");
+
+//Variables
+var continueOn = prompt("would you like to continue?");
+
+/*****************************
+Main code
+*****************************/
+
+while ( continueOn != "no"){
+    alert("continuing!")
+    continueOn = prompt("would you like to continue?");
+}
+
+alert("you are no longer continuing!")
+
+
+
+/***************************** 
+Functions 
+*****************************/
---
+++ t13_validation.js
@@ -0,0 +1,21 @@
+/*****************************
+ Name of task: Intro to JS and setting up your .js file.
+ Author: Angus. 
+ Date: Week 1 term 2 2025.   
+ purpose: Practice task.
+ ****************************/
+console.log("Running t01_introduction.js");
+
+//Variables
+
+
+/*****************************
+Main code
+*****************************/
+
+
+
+
+/***************************** 
+Functions 
+*****************************/
