7f9afe53aee895cfee54a9fdc219905024e55bb5
23080ad <23080ad@hvhs.school.nz>
2025-06-03 23:57:49 +0000

    Jun 3, 2025, 11:57 PM

--- t15_array_sorting.js
+++ t15_array_sorting.js
@@ -83,7 +83,7 @@ while (moneyIsInvalid) {
 }
 
 //Calcualte the most expensive chocolate the user can buy
-for (i = 0; i < chocolatesArray.length; i++ && userCash <= priceArray[i]){
+for (i = 0; i < chocolatesArray.length; i++ && userCash <= priceArray){
    avaliableChoc = priceArray[i]
 }
 
